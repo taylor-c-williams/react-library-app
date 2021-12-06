c1bacaa1e4e02010084d2b236b7cd446aa9d2543
Taylor <taylor.callan.williams@gmail.com>
2021-12-06 10:31:59 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import BookList from './views/Books/BookList'
+import BookDetail from './views/Books/BookDetail'
+import './App.css'
+
+function App() {
+  // TODO: Add routes to books & views
+  return (
+    <main className="container">
+      <h1>Library Catalog</h1>
+      <BookList />
+    </main>
+  )
+}
+
+export default App
